ebc692bd5f588bab9ae0623a771d3509aa37cfad
yuanfengcaichen <48538958+yuanfengcaichen@users.noreply.github.com>
2020-12-06 22:19:40 +0800

    1206 commit

--- project_jwt_auth/src/main/java/club/codeqi/bean/user/userController.java
+++ project_jwt_auth/src/main/java/club/codeqi/bean/user/userController.java
@@ -22,17 +22,17 @@ public class userController {
 
     @GetMapping("/userinfo")
     public Map loginuserinfo(HttpServletRequest request){
-        String header = request.getHeader("Authorization");
-        String token = header.replace("Bearer ","");
-        Payload<user> payload = JwtUtils.getInfoFromToken(token,prop.getPublicKey(), user.class);
-        user user = payload.getUserInfo();
-        //user.getPermissions();//1. 转为json，2. JsonUtil.tolist(json,permission.class)
-        user.setPermissions(JsonUtils.toList(JsonUtils.toString(user.getPermissions()), permission.class));
+//        String header = request.getHeader("Authorization");
+//        String token = header.replace("Bearer ","");
+//        Payload<user> payload = JwtUtils.getInfoFromToken(token,prop.getPublicKey(), user.class);
+//        user user = payload.getUserInfo();
+//        //user.getPermissions();//1. 转为json，2. JsonUtil.tolist(json,permission.class)
+//        user.setPermissions(JsonUtils.toList(JsonUtils.toString(user.getPermissions()), permission.class));
         Map map = new HashMap();
         map.put("result",200);
-        map.put("userinfo",user);
+        //map.put("userinfo",user);
         map.put("token",request.getHeader("Authorization"));
-        map.put("expire",payload.getExpiration());
+        //map.put("expire",payload.getExpiration());
         return map;
     }
 }
